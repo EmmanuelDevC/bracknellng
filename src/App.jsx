3edb04e353f605c0be87031552824c9c3b504a4f
Emmanuel-Dev-CEO <emmanuelbusiness584@gmail.com>
2025-01-14 22:17:30 +0100

    second-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import LandingPage from "./Pages/landingPage"
+import LandingPage from "./Pages/LandingPage.jsx"
 
 
 
