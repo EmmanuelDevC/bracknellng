4005d0397da31678819da8802347635d33bb3705
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-02-27 16:24:07 -0800

    commit

--- client/src/Pages/ContactusPage.jsx
+++ client/src/Pages/ContactusPage.jsx
@@ -24,7 +24,7 @@ const ContactUs = () => {
         const data = Object.fromEntries(formData);
 
         try {
-            const response = await fetch('http://bracknellng-server.vercel.app/send-email', {
+            const response = await fetch('https://bracknellngserver-pink.vercel.app/send-email', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data),
