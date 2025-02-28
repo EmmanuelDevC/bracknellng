91cdcefc05046d83b12ecca42d95235f543c79e2
Emmanuel-Dev-CEO <emmanueldev584@gmail.com>
2025-02-28 10:00:44 -0800

    backend commit

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.CLIENT_URL  || '*', 
+    origin: process.env.CLIENT_URL  || process.env.BACKEND_URL ||  '*', 
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
